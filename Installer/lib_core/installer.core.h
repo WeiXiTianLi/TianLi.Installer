fd82058f5e5010415760b73cf7f2847cb3540e32
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-01-01 19:35:40 +0800

    add:C API support for core functions

--- Installer/lib_core/installer.core.h
+++ Installer/lib_core/installer.core.h
@@ -1,4 +1,6 @@
 #pragma once
+
+#ifdef __cplusplus
 #include <string>
 #include <optional>
 #include <functional>
@@ -6,14 +8,19 @@
 #include <string_view>
 namespace tianli
 {
-#ifdef CAPI_CORE
-    void download(const char *url, const char *filename, void (*progress)(int, int));
-    void decompression(const char *file_path, const char *target_dir, void (*progress)(int, int));
-    void install(const char *files_dir, const char *install_dir, void (*progress)(int, int));
-    void registration(const char *key, const char *value);
-#endif
     std::optional<std::string> download(std::string_view url, std::filesystem::path file, std::function<void(int, int)> progress, std::function<void(bool)> result);
     std::optional<std::string> decompression(std::filesystem::path file, std::filesystem::path target_dir, std::function<void(int, int)> progress, std::function<void(bool)> result);
     std::optional<std::string> install(std::filesystem::path files_dir, std::filesystem::path install_dir, std::function<void(int, int)> progress);
     std::optional<std::string> registration(std::string_view key, std::string_view value);
 } // namespace tianli
+#endif
+
+#ifdef EXPORT_CORE
+extern "C"
+{
+    char *download(char *url, char *file, void (*progress)(int, int), void (*result)(bool));
+    char *decompression(char *file, char *target_dir, void (*progress)(int, int), void (*result)(bool));
+    char *install(char *files_dir, char *install_dir, void (*progress)(int, int));
+    char *registration(char *key, char *value);
+}
+#endif
