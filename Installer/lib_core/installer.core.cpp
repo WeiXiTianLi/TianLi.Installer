83a774bc02097d686614735f637ce1a98f1f0acd
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2024-01-01 20:37:49 +0800

    update:实现c接口

--- Installer/lib_core/installer.core.cpp
+++ Installer/lib_core/installer.core.cpp
@@ -23,3 +23,36 @@ namespace tianli
         return core::registration(key, value);
     }
 } // namespace tianli
+
+tianli_error *tianli_error::create()
+{
+    return new tianli_error();
+}
+
+void tianli_error::destroy(tianli_error *error)
+{
+    delete error;
+    error = nullptr;
+}
+
+bool download(char *url, char *file, void (*progress)(int, int), void (*result)(bool), tianli_error *error)
+{
+    if (auto res = tianli::download(url, file, progress, result); res.has_value())
+    {
+        auto message = res.value();
+        error->code = 1;
+        error->message = (char *)malloc(message.size() + 1);
+        if (error->message)
+        {
+            memcpy(error->message, message.data(), message.size());
+            error->message[message.size()] = '\0';
+        }
+        return false;
+    }
+    return true;
+}
+
+// EXPORT bool download(char *url, char *file, void (*progress)(int, int), void (*result)(bool), tianli_error *error);
+// EXPORT bool decompression(char *file, char *target_dir, void (*progress)(int, int), void (*result)(bool), tianli_error *error);
+// EXPORT bool install(char *files_dir, char *install_dir, void (*progress)(int, int), tianli_error *error);
+// EXPORT bool registration(char *key, char *value, tianli_error *error);
--- Installer/lib_core/installer.core.h
+++ Installer/lib_core/installer.core.h
@@ -1,26 +1,39 @@
 #pragma once
 
-#ifdef __cplusplus
-#include <string>
-#include <optional>
-#include <functional>
-#include <filesystem>
-#include <string_view>
-namespace tianli
-{
-    std::optional<std::string> download(std::string_view url, std::filesystem::path file, std::function<void(int, int)> progress, std::function<void(bool)> result);
-    std::optional<std::string> decompression(std::filesystem::path file, std::filesystem::path target_dir, std::function<void(int, int)> progress, std::function<void(bool)> result);
-    std::optional<std::string> install(std::filesystem::path files_dir, std::filesystem::path install_dir, std::function<void(int, int)> progress);
-    std::optional<std::string> registration(std::string_view key, std::string_view value);
-} // namespace tianli
-#endif
+// #ifdef __cplusplus
+// #include <string>
+// #include <optional>
+// #include <functional>
+// #include <filesystem>
+// #include <string_view>
+// namespace tianli
+// {
+//     std::optional<std::string> download(std::string_view url, std::filesystem::path file, std::function<void(int, int)> progress, std::function<void(bool)> result);
+//     std::optional<std::string> decompression(std::filesystem::path file, std::filesystem::path target_dir, std::function<void(int, int)> progress, std::function<void(bool)> result);
+//     std::optional<std::string> install(std::filesystem::path files_dir, std::filesystem::path install_dir, std::function<void(int, int)> progress);
+//     std::optional<std::string> registration(std::string_view key, std::string_view value);
+// } // namespace tianli
+// #endif
 
 #ifdef EXPORT_CORE
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __declspec(dllimport)
+#endif
+
 extern "C"
 {
-    char *download(char *url, char *file, void (*progress)(int, int), void (*result)(bool));
-    char *decompression(char *file, char *target_dir, void (*progress)(int, int), void (*result)(bool));
-    char *install(char *files_dir, char *install_dir, void (*progress)(int, int));
-    char *registration(char *key, char *value);
+    struct EXPORT tianli_error
+    {
+        int code = 0;
+        char *message = nullptr;
+
+        static tianli_error *create();
+        static void destroy(tianli_error *error);
+    };
+
+    EXPORT bool download(char *url, char *file, void (*progress)(int, int), void (*result)(bool), tianli_error *error);
+    EXPORT bool decompression(char *file, char *target_dir, void (*progress)(int, int), void (*result)(bool), tianli_error *error);
+    EXPORT bool install(char *files_dir, char *install_dir, void (*progress)(int, int), tianli_error *error);
+    EXPORT bool registration(char *key, char *value, tianli_error *error);
 }
-#endif
