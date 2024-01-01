4e4177eb71b3e19c5c067fb1197d35f080d27010
Yu SuiXian <genggode@outlook.com>
2024-01-01 11:31:06 +0000

    fix:修复core没暴露可调用接口的问题

--- Installer/lib_core/installer.core.h
+++ Installer/lib_core/installer.core.h
@@ -12,8 +12,8 @@ namespace tianli
     void install(const char *files_dir, const char *install_dir, void (*progress)(int, int));
     void registration(const char *key, const char *value);
 #endif
-    std::optional<std::string> download(std::string_view url, std::filesystem::path file, std::function<void(int, int)> progress);
-    std::optional<std::string> decompression(std::filesystem::path file, std::filesystem::path target_dir, std::function<void(int, int)> progress);
+    std::optional<std::string> download(std::string_view url, std::filesystem::path file, std::function<void(int, int)> progress, std::function<void(bool)> result);
+    std::optional<std::string> decompression(std::filesystem::path file, std::filesystem::path target_dir, std::function<void(int, int)> progress, std::function<void(bool)> result);
     std::optional<std::string> install(std::filesystem::path files_dir, std::filesystem::path install_dir, std::function<void(int, int)> progress);
     std::optional<std::string> registration(std::string_view key, std::string_view value);
 } // namespace tianli
